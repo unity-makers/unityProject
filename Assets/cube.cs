4857ad8d9ccbda0cba4bc73f8ecd7294a7169242
冯诗伟 <FengShiwei@FswdeMacBook-Pro.local>
2017-08-11 11:26:22 +0800

    real first

---
+++ Assets/cube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log ("hit sth");
+	}
+
+}
---
+++ Assets/sphere.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphere : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Floor") {
+			Debug.Log ("hit floor");
+		}
+	}
+}
